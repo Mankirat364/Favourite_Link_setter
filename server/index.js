ca9b495e0b794edfb6749012e11e22e87645d0d1
Mankirat364 <mankirat.techguild@gmail.com>
2025-03-26 15:13:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://favourite-link-setter-frontend.onrender.com",
     credentials: true
 }));
 
